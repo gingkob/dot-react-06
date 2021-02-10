1c2c4b82174917ef82e449dfe90babca9b3fb3fc
gingkob <malivlada@hotmail.com>
2021-02-10 01:55:29 +0100

    starting with syling

---
+++ src/components/Alert/Alert.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Alert.css';
+
+const Alert = ({ children, title, type }) => {
+  return (
+    <div className={`alert-wrapper ${type}`}>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  )
+}
+
+Alert.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element.isRequired
+  ]),
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired
+}
+
+export default Alert
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Alert from '../Alert/Alert';
 
 function App() {
   return (
-    <>
-    </>
+    <div className='wrapper'>
+      <Alert title="Items Not Added" type="error">
+        <div>Your items are out of stock.</div>
+      </Alert>
+    </div>
   );
 }
 
